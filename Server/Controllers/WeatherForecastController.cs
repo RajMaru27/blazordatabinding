258087d66e6a6b404aa28358b62bbe81776163ee
Raj Maru <raj.m@fortune4.in>
2023-05-26 18:10:13 +0530

    init

---
+++ Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,54 @@
+using BlazorWASMdatabinding.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWASMdatabinding.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        public IEnumerable<WeatherForecast> Get([FromBody] string searchText)
+        {
+            var forcasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            });
+            
+            if (!string.IsNullOrEmpty(searchText))
+            {
+                return forcasts
+                    .Where(f => f.Summary.ToLower().Contains(searchText.ToLower()))
+                    .ToArray();
+            }
+
+            return forcasts.ToArray();
+        }
+    }
+}
